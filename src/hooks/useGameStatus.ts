67791b4571cbeb6a65ec4c7b81f9a50c080dcb21
cheddar <nowaczewskijack@gmail.com>
2023-05-10 15:50:00 +0100

    add highscores state to useGameSatus for highscore board

--- src/hooks/useGameStatus.ts
+++ src/hooks/useGameStatus.ts
@@ -1,17 +1,102 @@
 import React from "react";
 import { ROWPOINTS } from "../setup";
 
-export const useGameStatus = (rowsCleared: number) => {
+export const useGameStatus = (rowsCleared: number, gameOver: boolean) => {
   const [score, setScore] = React.useState(0);
   const [rows, setRows] = React.useState(0);
   const [level, setLevel] = React.useState(1);
+  const [highScores, setHighScores] = React.useState<number[]>([]);
 
   React.useEffect(() => {
     if (rowsCleared > 0) {
-      setScore((prev) => prev + ROWPOINTS[rowsCleared - 1] * level);
-      setRows((prev) => prev + rowsCleared);
+      const rowPoints = ROWPOINTS[rowsCleared - 1] * level;
+      setScore((prevScore) => prevScore + rowPoints);
+      setRows((prevRows) => prevRows + rowsCleared);
     }
-  }, [rowsCleared]);
+  }, [rowsCleared, level]);
 
-  return { score, setScore, rows, setRows, level, setLevel };
+  React.useEffect(() => {
+    if (gameOver) {
+      const newScore = score;
+      if (newScore > 0) {
+        const newHighScores = [...highScores, newScore]
+          .sort((a, b) => b - a)
+          .slice(0, 10);
+        setHighScores(newHighScores);
+      }
+    }
+  }, [gameOver]);
+
+  return {
+    score,
+    setScore,
+    rows,
+    setRows,
+    level,
+    setLevel,
+    highScores,
+    setHighScores,
+  };
 };
+
+export default useGameStatus;
+
+// high scores of levels reached
+// import React from "react";
+// import { ROWPOINTS } from "../setup";
+
+// export const useGameStatus = (rowsCleared: number) => {
+//   const [score, setScore] = React.useState(0);
+//   const [rows, setRows] = React.useState(0);
+//   const [level, setLevel] = React.useState(1);
+//   const [highScores, setHighScores] = React.useState<number[]>([]);
+
+//   React.useEffect(() => {
+//     if (rowsCleared > 0) {
+//       const rowPoints = ROWPOINTS[rowsCleared - 1] * level;
+//       setScore((prevScore) => prevScore + rowPoints);
+//       setRows((prevRows) => prevRows + rowsCleared);
+
+//       const newScore = score + rowPoints;
+//       if (newScore > 0) {
+//         const newHighScores = [...highScores, newScore]
+//           .sort((a, b) => b - a)
+//           .slice(0, 10);
+//         setHighScores(newHighScores);
+//       }
+//     }
+//   }, [rowsCleared]);
+
+//   return {
+//     score,
+//     setScore,
+//     rows,
+//     setRows,
+//     level,
+//     setLevel,
+//     highScores,
+//     setHighScores,
+//   };
+// };
+
+// export default useGameStatus;
+
+// original
+
+// import React from "react";
+// import { ROWPOINTS } from "../setup";
+
+// export const useGameStatus = (rowsCleared: number) => {
+//   const [score, setScore] = React.useState(0);
+//   const [rows, setRows] = React.useState(0);
+//   const [level, setLevel] = React.useState(1);
+
+//   React.useEffect(() => {
+//     if (rowsCleared > 0) {
+//       setScore((prev) => prev + ROWPOINTS[rowsCleared - 1] * level);
+//       setRows((prev) => prev + rowsCleared);
+//     }
+//   }, [rowsCleared]);
+
+//   return { score, setScore, rows, setRows, level, setLevel };
+// };
