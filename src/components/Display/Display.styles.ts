c3a58250ab3a6942f651c6e02a959a342e9523fa
cheddar <nowaczewskijack@gmail.com>
2023-05-11 15:50:00 +0100

    add flex properties to display themes

--- src/components/Display/Display.styles.ts
+++ src/components/Display/Display.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 type Props = {
   gameOver?: boolean;
@@ -7,14 +7,16 @@ type Props = {
 export const StyledDisplay = styled.div<Props>`
   box-sizing: border-box;
   display: flex;
-  align-items: space-between;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   margin: 0 0 20px 0;
   padding: 20px;
   border: 2px solid #777;
   min-height: 20px;
   width: 120px;
   border-radius: 10px;
-  color: ${props => (props.gameOver ? 'red' : '#999')};
+  color: ${(props) => (props.gameOver ? "red" : "#999")};
   background: #000;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 0.8rem;
